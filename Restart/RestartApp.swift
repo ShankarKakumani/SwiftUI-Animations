5d632611f1cbc2dc6a101fa357066993c4160fa7
Shankar Kakumani <shankarchowdary7545@gmail.com>
2022-06-04 09:23:50 +0530

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Shankar Chowdary on 04/06/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
